fe55a141f3e000601f02968a7fb100f3e5a0f02b
Phillip <Phillip@Phillips-MacBook-Air.local>
2021-04-21 17:48:59 +1000

    Test
    Testing webscraper detection on streaming sites.

--- scraper.js
+++ scraper.js
@@ -1,7 +1,35 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin());
 
+puppeteer.launch({ headless: true }).then(async browser => {
+    console.log('Running tests..')
+    const page = await browser.newPage()
+    await page.goto('https://twist.moe/a/one-punch-man/1')
+    await page.waitForTimeout(5000)
+    await page.screenshot({ path: 'testresult.png', fullPage: true })
+    await browser.close()
+    console.log(`All done, check the screenshot. ✨`)
+  })
+
+
+/*
+async function scrapeVideo(url){
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector('video');
+    //const layout = await page.querySelector("video");
+
+    //console.log(layout);
+}
+
+
+scrapeVideo("https://twist.moe/a/one-punch-man/1");
+*/
+/*
 async function scrapePage(url){
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless:true});
     const page = await browser.newPage();
     await page.goto(url);
 
@@ -14,6 +42,8 @@ async function scrapePage(url){
     
     const [e2] = await page.$x('//*[@id="productTitle"]');
     const txt = await e2.getProperty('textContent');
+    console.log(txt);
+    
     const rawText = await txt.jsonValue();
 
     console.log({srcText,rawText});
@@ -21,3 +51,5 @@ async function scrapePage(url){
 }
 
 scrapePage('https://www.amazon.com/HiFiHear-Earphone-Diaphragm-Headphone-Detachable/dp/B07TZ2NQCF');
+
+*/
