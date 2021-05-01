c5430a142e0347464ab4dfbf42eb75539b02fb5c
Phillip <Phillip@Phillips-MacBook-Air.local>
2021-05-01 18:44:24 +1000

    Update scraper.js
    General cleaning of code and some comments added.

--- scraper.js
+++ scraper.js
@@ -7,11 +7,19 @@ const AdBlockerPlugin = require('puppeteer-extra-plugin-adblocker');
 
 puppeteer.launch({headless:true}).then(async browser => {
 
-    // opening a new page and navigating to Reddit
+    // Open new page in browser
     const page = await browser.newPage();
+
+    // Navigate to URL on the new page
     await page.goto('https://twist.moe/a/one-punch-man/1');
+
+    // Wait for video tag to load on the website
     await page.waitForSelector('video');
+
+    // Wait for 1000 milliseconds -- this was to prevent scraper from returning null for whatever reason
     await page.waitForTimeout(1000);
+
+    
     const vidSource = await page.evaluate(() => {
         let title = document.querySelector('.series-title > span').innerHTML.trim();
         let episode = document.querySelector('.series-episode > span').innerHTML;;
