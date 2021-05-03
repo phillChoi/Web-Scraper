14e54d5177a2b450efa1cbb17bd40e55cf72c106
Phillip <Phillip@Phillips-MacBook-Air.local>
2021-05-03 21:44:01 +1000

    Update scraper.js
    Split the the scraper to work as two seperate function, getEpisodeList() retrieves the website links of all the epsiodes and returns it as an object with {epSource, episodeNum} and getEpisodeDetails requires an array to iterate through the list and navigate to each link and retrieve AnimeTitle, Episode and Source.

--- scraper.js
+++ scraper.js
@@ -1,63 +1,108 @@
-const puppeteer = require('puppeteer');
-const randomUseragent = require('random-useragent');
+const puppeteer = require('puppeteer-extra');
+//const randomUseragent = require('random-useragent');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const AdBlockerPlugin = require('puppeteer-extra-plugin-adblocker');
-//puppeteer.use(StealthPlugin());
-//puppeteer.use(AdBlockerPlugin({ blockTrackers: true}));
-
-puppeteer.launch({headless:true}).then(async browser => {
-
-    // Open new page in browser
-    const page = await browser.newPage();
-
-    // Navigate to URL on the new page
-    await page.goto('https://twist.moe/a/one-punch-man/1');
-
-    // Wait for video tag to load on the website
-    await page.waitForSelector('video');
-
-    // Wait for 5 seconds -- this was to prevent scraper from returning null for whatever reason
-    await page.waitForTimeout(5000);
+puppeteer.use(StealthPlugin());
+puppeteer.use(AdBlockerPlugin({ blockTrackers: true}));
 
 
-    const vidSource = await page.evaluate(() => {
-        // Get an array of all the episodes for the anime
-        let allEpisodes = document.querySelectorAll('.episode-list.show-all > ul > li');
-       
-        //Create an array to store information
-        episodeList = [];
+async function getEpisodeList() {
+    try {
+        //puppeteer.launch({headless:true}).then(async browser => {
+        const browser = await puppeteer.launch({headless:true});
 
-        //Iterate through all the listed episodes 
-        allEpisodes.forEach(episode => {
-            let episodeSource = episode.querySelector('a').getAttribute('href');
-            let episodeNum = episode.querySelector('a span').innerHTML;
+        // Open new page in browser
+        const page = await browser.newPage();
 
-            episodeList.push({
-                epSource: episodeSource,
-                epNo: episodeNum,
-            })
+        // Navigate to URL on the new page
+        await page.goto('https://twist.moe/a/one-punch-man/1');
+
+        // Wait for video tag to load on the website
+        await page.waitForSelector('video');
+
+        // Wait for 5 seconds -- this was to prevent scraper from returning null for whatever reason
+        await page.waitForTimeout(5000);
+
+
+        const vidSource = await page.evaluate(() => {
+            // Get an array of all the episodes for the anime
+            let allEpisodes = document.querySelectorAll('.episode-list.show-all > ul > li');
+        
+            //Create an array to store information
+            episodeArray = [];
+
+            //Iterate through all the listed episodes 
+            allEpisodes.forEach(episode => {
+                let episodeSource = episode.querySelector('a').getAttribute('href');
+                let episodeNum = episode.querySelector('a span').innerHTML;
+
+                episodeArray.push({
+                    epSource: ('https://twist.moe'+episodeSource),
+                    epNo: episodeNum,
+                });
+            });
+            return episodeArray;
         });
+        await browser.close();
+        console.log(vidSource);
+        return vidSource;
+    } catch(err){
+        console.log(err);
+    } 
+};
 
-        /*
+async function getEpisodeDetails(array){
+    /* try { */
+        const browser = await puppeteer.launch({headless:true});
+        const page = await browser.newPage();
+        episodeDetails = [];
+        
+        for (x = 0; x<array.length;x++){
+            let link = array[x];
+            await page.goto(link.epSource);
+            await page.waitForTimeout(1000);
+            //console.log("Now at: "+link.epSource);
+            let title = page.querySelector('.series-title > span').innerHTML.trim();
+            let episode = page.querySelector('.series-episode > span').innerHTML;;
+            let vidSrc = page.querySelector('video').getAttribute('src');
+            episodeDetails.push({
+                AnimeTitle: title,
+                Episode: episode,
+                Source: vidSrc,
+            });
+        };
+        await browser.close();
+        console.log("Finished iterating through pages.");
+        console.log(episodeDetails);
+        /* 
         let title = document.querySelector('.series-title > span').innerHTML.trim();
         let episode = document.querySelector('.series-episode > span').innerHTML;;
         let vidSrc = document.querySelector('video').getAttribute('src');
 
-        episodeDetails = [];
+        
         episodeDetails.push({
             AnimeTitle: title,
             Episode: episode,
             Source: vidSrc,
         });
-        return episodeDetails;
-        
-        */
-        return episodeList;
-    });
-    await browser.close();
-    console.log(vidSource);
-    //console.log(vidSource);
+        return episodeDetails; */
+    /* } catch (err){
+        console.log(err); 
+    }*/
+};
+let episodeArray = [
+    { epSource: 'https://twist.moe/a/one-punch-man/1', epNo: '1' },
+    { epSource: 'https://twist.moe/a/one-punch-man/2', epNo: '2' },
+    { epSource: 'https://twist.moe/a/one-punch-man/3', epNo: '3' },
+    { epSource: 'https://twist.moe/a/one-punch-man/4', epNo: '4' },
+    { epSource: 'https://twist.moe/a/one-punch-man/5', epNo: '5' },
+    { epSource: 'https://twist.moe/a/one-punch-man/6', epNo: '6' },
+    { epSource: 'https://twist.moe/a/one-punch-man/7', epNo: '7' },
+    { epSource: 'https://twist.moe/a/one-punch-man/8', epNo: '8' },
+    { epSource: 'https://twist.moe/a/one-punch-man/9', epNo: '9' },
+    { epSource: 'https://twist.moe/a/one-punch-man/10', epNo: '10' },
+    { epSource: 'https://twist.moe/a/one-punch-man/11', epNo: '11' },
+    { epSource: 'https://twist.moe/a/one-punch-man/12', epNo: '12' }
+  ];
 
-}).catch(function (err) {
-    console.error(err);
-});
+getEpisodeDetails(episodeArray);
